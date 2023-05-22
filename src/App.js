c3153ef93bb6953dc7a60c502d23e79274390bcf
ashish-singh96 <ashishbhadauriya5344@gmail.com>
2023-05-22 15:57:53 +0530

    Done Counter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
+import Counter from './component/Counter';
 function App() {
   return (
-    <div>
-      
+    <div className='App'>
+      <Counter/>
     </div>
   );
 }
--- src/component/Counter.js
+++ src/component/Counter.js
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Counter = () => {
+    const[count,setCount]=useState(0);
+    const[desc,setDesc]=useState(0);
+     const handleIncre = () =>{
+        setCount(count+1);
+     }
+
+     const handleDesc = () =>{
+        setDesc(desc-1);
+     }
+   //Method 2nd State
+   const[value,setValue]=useState(0);
+   const handelIn = () =>{
+    setValue(value+1);
+   }
+
+   const handleDes =() =>{
+    setValue(value-1);
+   }
   return (
-    <div>Counter</div>
+    <div>
+        <h4>Increment:{count}</h4>
+        <button className='btn btn-primary' onClick={(e)=>handleIncre(e)}>+</button>
+        <h4>Decrement:{desc}</h4>
+        <button className='btn btn-primary' onClick={(e)=>handleDesc(e)}>-</button>
+
+        <h3 className='text-danger'>Method 2 </h3>
+
+        <button className='btn btn-primary' onClick={(e)=>handelIn(e)}>Increment</button>
+        <h3>Value:{value}</h3>
+        <button className='btn btn-primary' onClick={(e)=>handleDes(e)}>Decrement</button>
+    </div>
   )
 }
 
