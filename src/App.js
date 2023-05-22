a263e458c2a085cd49fe4f07aba5af3f9e98c447
ashish-singh96 <ashishbhadauriya5344@gmail.com>
2023-05-22 15:45:48 +0530

    Counter App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import './App.css';
-import Counter from './Components/Counter';
-import Hooks from './Components/Hooks';
-import Parent from './Components/Parent';
-
 function App() {
   return (
     <div>
-      <Hooks/>
+      
     </div>
   );
 }
