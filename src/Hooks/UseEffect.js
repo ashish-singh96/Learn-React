c856a8deed30edececc75a4718d349fb55f01eba
ashish-singh96 <ashishbhadauriya5344@gmail.com>
2023-05-23 13:01:36 +0530

    useEffect

--- src/Hooks/UseEffect.js
+++ src/Hooks/UseEffect.js
@@ -2,15 +2,19 @@ import React, { useEffect, useState } from 'react'
 
 const UseEffect = () => {
     const[count,setCount]=useState(0);
+    const[calculate,SetCalculate]=useState(0);
 
     useEffect(()=>{
         setTimeout(() => {
-            setCount(()=>count+1);
-        }, 1000);
-    })
+            SetCalculate(()=>2*count);
+        });
+    },[count])
+
   return (
     <div>
         <h3>Count:{count}</h3>
+        <button className='btn btn-primary' onClick={()=>setCount((count)=>count+1)}>Click me </button>
+        <p>Calculation:{calculate}</p>
     </div>
   )
 }
