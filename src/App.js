366d60cb27db233896223863e3bf5d76b8c44e30
ashish-singh96 <ashishbhadauriya5344@gmail.com>
2023-05-24 14:02:49 +0530

    Shoes product

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import UseEffect from './Hooks/UseEffect';
 import UseState from './Hooks/UseState';
+import Card from './component/Card';
 import Counter from './component/Counter';
 import Forms from './component/Forms';
 import Parent from './component/Parent';
@@ -8,8 +9,7 @@ import Table from './component/Table';
 function App() {
   return (
     <div className='App'>
-      <Table/>
-      <Forms/>
+      <Card/>
     </div>
   );
 }
